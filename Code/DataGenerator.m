1dfc600c9f789885336a8a8e282047139e91a944
Shashi Kant <shashikg.iitk@gmail.com>
2018-11-14 19:05:13 +0530

    added file for generating the data for model

---
+++ Code/DataGenerator.m
@@ -0,0 +1,24 @@
+% Generate required data ------------------------------------------------------
+clear;
+
+Xa = [16.666,50,83.333,33.333,66.666,16.666,50,83.333,33.33,66.66,16.666,50,83.33];
+Ya = [16.666,16.666,16.666,33.333,33.333,50,50,50,66.66,66.66,83.333,83.33,83.33];
+
+x_actual = zeros(113, 2);
+x_actual(1:100,:) = 100.*rand(100,2);
+x_actual(101:113,:) = [transpose(Xa), transpose(Ya)];
+
+x_observed = zeros(113, 2);
+x_observed(1:100,:) = x_actual(1:100,:) + 10.0.*randn(100, 2);
+x_observed(101:113,:) = x_actual(101:113,:) + 0.1.*randn(13, 2);
+
+h_actual = zeros(113,113);
+for i = 1:113
+  for j = 1:113
+    h_actual(i,j) = norm(x_actual(i,:) - x_actual(j,:));
+  end
+end
+
+h_observed = h_actual + 1.*randn(113, 113);
+
+save data.mat

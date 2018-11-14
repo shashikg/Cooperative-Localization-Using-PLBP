fcd1281d0bb63cd7cb06713006020ed0260e336c
Shashi Kant <shashikg.iitk@gmail.com>
2018-11-15 00:16:04 +0530

    Added a function to plot graph

---
+++ Code/plotGraph.m
@@ -0,0 +1,12 @@
+function plotGraph(V, E)
+  for i = 1:113
+    for j = 1:113
+      if E(i,j)
+        hold on;
+        plot([V(i,1) V(j,1)], [V(i,2) V(j,2)], 'o-b', 'LineWidth', 1);
+      end
+    end
+  end
+  plot(V(101:113,1), V(101:113,2), 'xr', 'LineWidth', 5);
+  grid on
+end

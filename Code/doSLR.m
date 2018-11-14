c0b46f60f85cb86ae75f4d5726e0edf7c7717aed
Shashi Kant <shashikg.iitk@gmail.com>
2018-11-15 01:00:44 +0530

    SLR algo done

---
+++ Code/doSLR.m
@@ -0,0 +1,33 @@
+function [A, b, sigma] = doSLR(_ul, _Wl)
+  N = 4;
+  X = zeros(4,9);
+  % Sigma Points and Corresponding Weight Generation --------------------------
+  X(:,1) = _ul;
+  w1 = 1/3;
+  wo = (1-w1)./(2.*N);
+  T = chol(_Wl);
+  f = (N/(1-w1))^(1/2);
+  for i=2:5
+    X(:,1) = _ul + f.*(T(i-1,:)');
+    X(:,i+N) = _ul - f.*(T(i-1,:)');
+  end
+  
+  Z = sqrt((X(1,:) - X(3,:)).^2 + (X(2,:) - X(4,:)).^2);
+  z = w1.*Z(:,1) + wo.*sum(Z(:,2:9));
+  
+  shi = w1.*(X(:,1) - _ul).*(Z(:,1) - z);
+  for j=2:9
+    shi = shi + wo.*(X(:,j) - _ul).*(Z(:,j) - z);
+  end
+  
+  phi = w1.*(Z(:,1) - z).*(Z(:,1) - z);
+  for j=2:9
+    phi = phi + w1.*(Z(:,j) - z).*(Z(:,j) - z);
+  end
+  
+  A = (shi')*(_Wl^(-1));
+  b = z - A*_ul;
+  sigma = phi - A*_Wl*(A');
+end
+
+  
